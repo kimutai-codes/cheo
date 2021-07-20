2e0a3e552ef63b97a16a748eb435e3920131d98e
kimutai-codes <allankimutaikoech@gmail.com>
2021-07-20 12:04:24 +0300

    loading

--- src/pages/Feeds.js
+++ src/pages/Feeds.js
@@ -1,32 +1,46 @@
 // import { Button, List } from 'antd';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { LoadingOutlined } from '@ant-design/icons';
 
-const getFeeds = () => {
-	return fetch('http://192.168.8.101:3000/api/feeds/').then((res) =>
-		res.json()
-	);
-};
+const url = 'http://192.168.8.101:3000/api/feeds/';
 
 const Feeds = () => {
 	const [feeds, setFeeds] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		getFeeds().then((feeds) => setFeeds(feeds));
-	});
+		axios
+			.get(url)
+			.then((res) => setFeeds(res.data))
+			.then(() => setLoading(false))
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
 	return (
 		<div>
 			<h1>This Is Feeds</h1>
 			<ul>
-				{feeds.map((feed) => {
-					const feedName = feed.feed_name;
-					const feedPrice = feed.feed_price;
-					return (
-						<li key={feed.id} className='disp'>
-							<h2>{feedName}</h2> <h3>{feedPrice}</h3>
-						</li>
-					);
-				})}
+				{loading ? (
+					<div className='loading'>
+						<h1>Loading ...</h1>
+						<LoadingOutlined
+							style={{ fontSize: '50px', color: '#1890ff' }}
+						/>
+					</div>
+				) : (
+					feeds.map((feed) => {
+						const feedName = feed.feed_name;
+						const feedPrice = feed.feed_price;
+						return (
+							<li key={feed.id} className='disp'>
+								<h2>{feedName}</h2> <h3>{feedPrice}</h3>
+							</li>
+						);
+					})
+				)}
 			</ul>
 		</div>
 	);
