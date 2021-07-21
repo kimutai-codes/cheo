3fbcb9ac2802a8fa7d430a47b025c00e8aeb070d
kimutai-codes <allankimutaikoech@gmail.com>
2021-07-21 20:21:47 +0300

    redirect working

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,55 +1,65 @@
 import { Form, Input, Button, PageHeader } from 'antd';
 import axios from 'axios';
 import { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/header';
 
 const Login = () => {
-const [fail,setFail]= useState('')
-const [success,setSuccess]=useState(false)
+	const [fail, setFail] = useState('');
+	const [success, setSuccess] = useState(false);
 
-const url = 'http://192.168.8.101:3000/api/feeders/login';
+	const url = 'http://192.168.8.101:3000/api/feeders/login';
 
-const onFinish = (values) => {
-	axios.post(url, values).then((res) => {
-    if(!res.data.length ){
-      setFail(res.data.message)
-    }else{
-      setSuccess(true)
-    }
-  });
-};
+	const onFinish = (values) => {
+		axios.post(url, values).then((res) => {
+			if (!res.data.length) {
+				setFail(res.data.message);
+			} else {
+				setSuccess(true);
+			}
+		});
+	};
 	return (
 		<div className='form-wrapper'>
-      {
-        success?<div>Redirect</div>:(<div>
+			{success ? (
+				<div>
+					<Redirect to='/daychart' />
+				</div>
+			) : (
+				<div>
+					<PageHeader className='loginheader' />
+					<h1>Hello, Please Log In</h1>
+					<Form onFinish={onFinish}>
+						<Form.Item name='first_name'>
+							<Input
+								placeholder='Enter First Name'
+								required
+							></Input>
+						</Form.Item>
 
-			<PageHeader className='loginheader' />
-			<h1>Hello, Please Log In</h1>
-			<Form onFinish={onFinish}>
-				<Form.Item name='first_name'>
-					<Input placeholder='Enter First Name' required></Input>
-				</Form.Item>
+						<Form.Item name='second_name'>
+							<Input
+								placeholder='Enter Second Name'
+								required
+							></Input>
+						</Form.Item>
 
-				<Form.Item name='second_name'>
-					<Input placeholder='Enter Second Name' required></Input>
-				</Form.Item>
+						<Form.Item name='feeder_password'>
+							<Input.Password
+								placeholder='Enter Password'
+								required
+							></Input.Password>
+						</Form.Item>
 
-				<Form.Item name='feeder_password'>
-					<Input.Password
-						placeholder='Enter Password'
-						required
-					></Input.Password>
-				</Form.Item>
-
-				<Form.Item>
-					<Button type='primary' htmlType='submit'>
-						Login
-					</Button>
-				</Form.Item>
-			</Form>
-      <div className="fail">{fail}</div>
-        </div> )
-      }
+						<Form.Item>
+							<Button type='primary' htmlType='submit'>
+								Login
+							</Button>
+						</Form.Item>
+					</Form>
+					<div className='fail'>{fail}</div>
+				</div>
+			)}
 		</div>
 	);
 };
