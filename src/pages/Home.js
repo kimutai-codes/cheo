208ad167f2effe04aa09b3156bf8a6021a73061a
kimutai-codes <allankimutaikoech@gmail.com>
2021-07-22 21:49:54 +0300

    working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,29 @@
 import { Button } from 'antd';
+import { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 const Home = () => {
+	const [wantreg, setWantreg] = useState(false);
+	const [wantadmin, setWantAdmin] = useState(false);
+	const reg = () => {
+		setWantreg(true);
+	};
+	const admn = () => {
+		setWantAdmin(true);
+	};
+	console.log(wantreg);
 	return (
 		<div>
 			<h1>Welcome</h1>
 			<h2>Who Would You Like To Sign In As</h2>
-			<Button type='primary' className='homeBtn'>
+			<Button type='primary' className='homeBtn' onClick={reg}>
 				User Login
 			</Button>
-			<Button type='primary' className='homeBtn'>
+			<Button type='primary' className='homeBtn' onClick={admn}>
 				Admin Login
 			</Button>
+			{wantreg ? <Redirect to='/login' /> : null}
+			{wantadmin ? <Redirect to='/admin' /> : null}
 		</div>
 	);
 };
